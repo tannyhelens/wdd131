79eb306b22cb87ee11ff088d644d2e3f39a59e3c
Tanny Santos <tannyhelens@gmail.com>
2026-05-15 19:56:24 -0600

    added responsive hamburger menu

--- scripts/temples.js
+++ scripts/temples.js
@@ -8,3 +8,13 @@ title.addEventListener("click", function () {
 
 document.getElementById("lastModified").textContent =
 "Last Modification: " + document.lastModified;
+
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector("nav");
+
+hamButton.addEventListener("click", () => {
+
+    navigation.classList.toggle("open");
+    hamButton.classList.toggle("open");
+
+});
