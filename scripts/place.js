14b95545059f7c12262470835c93590d82606237
Tanny Santos <tannyhelens@gmail.com>
2026-05-22 22:43:14 -0600

    Clean up wind chill script

--- scripts/place.js
+++ scripts/place.js
@@ -1,30 +1,17 @@
 document.getElementById("currentyear").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent =
-  "Last Modification: " + document.lastModified;
+    "Last Modification: " + document.lastModified;
 
-  const temperature = 8; // °C
+const temperature = 8; // °C
 const windSpeed = 10; // km/h
 
 function calculateWindChill(temp, speed) {
-    return (
-        13.12 +
-        0.6215 * temp -
-        11.37 * Math.pow(speed, 0.16) +
-        0.3965 * temp * Math.pow(speed, 0.16)
-    ).toFixed(1);
+    return (13.12 + 0.6215 * temp - 11.37 * Math.pow(speed, 0.16) + 0.3965 * temp * Math.pow(speed, 0.16)).toFixed(1);
 }
 
-// mostrar wind chill só se condição for válida
 if (temperature <= 10 && windSpeed > 4.8) {
     document.getElementById("windchill").textContent =
         calculateWindChill(temperature, windSpeed) + " °C";
 } else {
     document.getElementById("windchill").textContent = "N/A";
 }
-
-// Footer
-document.getElementById("currentyear").textContent =
-    new Date().getFullYear();
-
-document.getElementById("lastModified").textContent =
-    "Last Modification: " + document.lastModified;
